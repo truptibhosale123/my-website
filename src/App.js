cc9f262005b945fefed99054549ff5410347677d
truptibhosale123 <bhosale.trupti@gmail.com>
2024-09-04 12:19:16 +0530

    next commit

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
       <h1>About Me</h1>
         <p>Hello! My name is Trupti Bhosale. I am a software developer with 17 years of experience.</p>
         <p>I specialize in C#.Net, ASP.Net, .Net Core, React, and SQL Server.</p>
+        <p>I am also an ultra trail runner.</p>
       </header>
     </div>
   );
